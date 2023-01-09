4660e5d648b92b11deb248d0dec0d5180232c13d
66945 <dtmcgra@gmail.com>
2023-01-09 10:15:31 -0700

    Initial Commit

---
+++ Flashlight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight2
+//
+//  Created by McGrath, Daniel - Student on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
